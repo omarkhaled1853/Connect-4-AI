99254e6bc6180c36286ca4652bf1e093600ac443
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-01 19:09:46 +0200

    Adding alpha and beta to Node

--- node.py
+++ node.py
@@ -1,8 +1,12 @@
+import math
+
 class Node:
     def __init__(self, board: list[list[chr]]) -> None:
         self.__board = board
         self.__children = []
         self.__heurstic_value = 0
+        self.__alpha = -math.inf
+        self.__beta = math.inf
     
     def get_board(self) -> list[list[chr]]:
         return self.__board
@@ -18,3 +22,15 @@ class Node:
 
     def set_heuristic_value(self, heurstic_value: int):
         self.__heurstic_value = heurstic_value
+    
+    def get_alpha(self) -> int:
+        return self.__alpha
+    
+    def set_alpha(self, alpha: int):
+        self.__alpha = alpha
+    
+    def get_beta(self) -> int:
+        return self.__beta
+    
+    def set_beta(self, beta: int):
+        self.__beta = beta
