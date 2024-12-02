a4129f920db57b45effb6676c655d8a47bf9999d
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-02 18:20:10 +0200

    Combining Factory class with search algorithms

--- utils/factory.py
+++ utils/factory.py
@@ -1,3 +1,7 @@
+from Algorithms.search import Search
+from Algorithms.minimax import Minimax
+from Algorithms.minimax_alpha_beta import Minimax_alpha_beta
+from Algorithms.expected_minimax import Expected_minimax
 
 # factory class to returun specific search alogrithm
 class Factory:
@@ -6,8 +10,8 @@ class Factory:
                        depth: int = 42, rows: int = 6, cols: int = 7) -> Search:
         if algorithm_name == 'Minimax':
             return Minimax(agent_piece, human_piece, depth, rows, cols)
-        elif algorithm_name == 'MiniMax_alpha_beta':
-            return MiniMax_alpha_beta(agent_piece, human_piece, depth, rows, cols)
+        elif algorithm_name == 'Minimax_alpha_beta':
+            return Minimax_alpha_beta(agent_piece, human_piece, depth, rows, cols)
         elif algorithm_name == 'Expected_minimax':
             return Expected_minimax(agent_piece, human_piece, depth, rows, cols)
         else: None
